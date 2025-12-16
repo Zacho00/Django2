eb5100f48c91e900a5df67333aed9a242a3a87aa
Zach Thomas <zthomas36@ivytech.edu>
2025-12-15 20:04:48 -0500

    fully working

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .models import Comment
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -15,6 +22,23 @@ def post_detail(request, pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
 
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {"form":form})
+
+@login_required   
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -28,3 +52,49 @@ def post_new(request):
     else:
         form = PostForm()
         return render(request, 'blog/post_edit.html', {"form":form})
+
+@login_required  
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts' : posts})
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+    return redirect('post_list')
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('post_list')
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+@login_required   
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = CommentForm()
+        return render(request, 'blog/add_comment_to_post.html', {"form":form})
